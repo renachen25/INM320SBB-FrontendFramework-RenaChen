a6c627e90a86173140e91248884c352d26a397e0
renachen25 <chenxyx25@gmail.com>
2025-11-11 00:18:13 -0500

    Adding card and graph

---
+++ base/assets/js/main.js
@@ -0,0 +1,63 @@
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx,{
+    type: 'line',
+    data: {
+        labels: [
+            '0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22'
+        ],
+        datasets: [
+            {
+                label: 'Today',
+                data: [
+                    15,21,26,28,29,28,33,45,51,42,25,19,18,22,38,46,48,46,42,38
+                ],
+                lineTension: 1/3,
+                backgroundColor: "transparent",
+                borderColor:'#284DFF',
+                borderWidth: 2,
+                pointRadius: 0
+            },
+            {
+                label: 'Yesterday',
+                data: [
+                    33,34,31.4,24,22.2,24.2,30,32.5,33,31.5,27,22,17.9,17.5,26,42,34,30,31.5,35
+                ],
+                lineTension: 1/3,
+                backgroundColor: "transparent",
+                borderColor:'#E4E5EF',
+                borderWidth: 2,
+                pointRadius: 0
+            }
+        ],
+    },
+    options: {
+        scales: {
+            x: {
+                grid: {
+                    drawOnChartArea: false,  
+                    drawTicks: true
+                  }
+            },
+            y: {
+                position: 'right',
+                suggestedMin: 0,
+                suggestedMax: 60,
+                grid: {
+                    drawBorder: false, 
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                align: 'end',
+                labels: {
+                    usePointStyle: true,
+                    pointStyle: 'line',
+                    boxWidth: 25,
+                    boxHeight: 40
+                },
+            }
+        }
+    }
+})
