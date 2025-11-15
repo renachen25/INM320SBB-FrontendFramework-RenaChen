5f63abc19adf39f853f3224d8de388757fdf78c7
renachen25 <chenxyx25@gmail.com>
2025-11-15 12:44:25 -0500

    Move style to CSS

--- base/assets/js/main.js
+++ base/assets/js/main.js
@@ -1,20 +1,21 @@
+// Create chart
 const ctx = document.getElementById("myChart");
 
-new Chart(ctx,{
+new Chart(ctx, {
     type: "line",
     data: {
         labels: [
-            "0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22"
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22"
         ],
         datasets: [
             {
                 label: "Today",
                 data: [
-                    15,21,26,28,28.5,28,30,44,51,42,25,19,18,22,38,46,48,46,42,38
+                    15, 21, 26, 28, 28.5, 28, 30, 44, 51, 42, 25, 19, 18, 22, 38, 46, 48, 46, 42, 38
                 ],
-                lineTension: 1/3,
+                lineTension: 1 / 3,
                 backgroundColor: "transparent",
-                borderColor:"#284DFF",
+                borderColor: "#284DFF",
                 borderWidth: 2,
                 pointRadius: 0.3,
                 pointHoverRadius: 6,
@@ -24,11 +25,11 @@ new Chart(ctx,{
             {
                 label: "Yesterday",
                 data: [
-                    33,34,31.4,24,22.2,24.2,30,32.5,33,31.5,27,22,17.9,17.5,26,42,34,30,31.5,35
+                    33, 34, 31.4, 24, 22.2, 24.2, 30, 32.5, 33, 31.5, 27, 22, 17.9, 17.5, 26, 42, 34, 30, 31.5, 35
                 ],
-                lineTension: 1/3,
+                lineTension: 1 / 3,
                 backgroundColor: "transparent",
-                borderColor:"#E4E5EF",
+                borderColor: "#E4E5EF",
                 borderWidth: 2,
                 pointRadius: 0.3,
                 pointHoverRadius: 6,
@@ -41,16 +42,16 @@ new Chart(ctx,{
         scales: {
             x: {
                 grid: {
-                    drawOnChartArea: false,  
+                    drawOnChartArea: false,
                     drawTicks: true
-                  }
+                }
             },
             y: {
                 position: "right",
                 suggestedMin: 0,
                 suggestedMax: 60,
                 grid: {
-                    drawBorder: false, 
+                    drawBorder: false,
                 }
             }
         },
@@ -76,7 +77,7 @@ new Chart(ctx,{
                 bodyFont: { size: 12 },
                 displayColors: false,
                 padding: 8,
-              }
+            }
         }
     }
 })
