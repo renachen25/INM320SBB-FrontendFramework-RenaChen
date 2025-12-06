dff8dace87b9a5608e64022ddd27a5dc9d7ab580
renachen25 <chenxyx25@gmail.com>
2025-12-05 23:27:09 -0500

    Updated for headless CMS

--- base/assets/js/main.js
+++ base/assets/js/main.js
@@ -81,3 +81,151 @@ new Chart(ctx, {
         }
     }
 })
+
+// async/await
+async function getData() {
+    try {
+        const moduleData = await fetch('/assets/data/content.json');
+        const data = await moduleData.json();
+        // Unresolved Tickets Module
+        // Module Title
+        const unresolvedTitle = document.querySelector('#unresolvedTitle');
+        unresolvedTitle.innerHTML = data.unresolvedTicketModule.title;
+        // Module Action
+        const unresolvedAction = document.querySelector('#unresolvedAction');
+        unresolvedAction.innerHTML = data.unresolvedTicketModule.action;
+        // Module Label
+        const unresolvedLabel = document.querySelector('#unresolvedLabel');
+        unresolvedLabel.innerHTML = data.unresolvedTicketModule.label;
+        // Module Label Details
+        const unresolvedLabelDetails = document.querySelector('#unresolvedLabelDetails');
+        unresolvedLabelDetails.innerHTML = data.unresolvedTicketModule.labelDetails;
+        // Module List
+        const unresolvedList = document.querySelector('#unresolvedList');
+        data.unresolvedTicketModule.listInfo.forEach(item => {
+            const newListItem = document.createElement('li');
+            newListItem.className = 'list-group-item d-flex justify-content-between align-items-center px-0';
+            newListItem.innerHTML = `
+                <span>${item.name}</span>
+                <strong class="small fw-medium text-body-tertiary">${item.count}</strong>
+            `;
+            unresolvedList.appendChild(newListItem);
+        });
+
+        // Tasks Module
+        // Module Title
+        const tasksTitle = document.querySelector('#tasksTitle');
+        tasksTitle.innerHTML = data.tasksModule.title;
+        // Module Action
+        const tasksViewDetails = document.querySelector('#tasksAction');
+        tasksViewDetails.innerHTML = data.tasksModule.action;
+        // Module Section Label
+        const tasksSectionLabel = document.querySelector('#tasksSectionLabel');
+        tasksSectionLabel.innerHTML = data.tasksModule.sectionLabel;
+        // Module List
+        const tasksList = document.querySelector('#tasksList');
+        data.tasksModule.tasksList.forEach(item => {
+            const newListItem = document.createElement('li');
+            newListItem.className = "list-group-item d-flex align-items-center justify-content-between px-0";
+            // Create New Task row
+            if (item.type === "new_task") {
+                newListItem.innerHTML = `
+                    <div class="d-flex align-items-center gap-2">
+                        <input type="text" class="form-control form-control-sm border-0 px-0" placeholder="${item.placeholder}">
+                    </div>
+                    <button class="custom-btn btn btn-sm p-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-${item.buttonIcon}" viewBox="0 0 16 16">
+                            <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0"/>
+                        </svg>
+                    </button>
+                `;
+            }
+            // Normal Task row
+            else if (item.type === "task") {
+                // Badges
+                let badgeClass = "badge";
+                if (item.badge.style === "warning") badgeClass += " bg-warning text-white";
+                if (item.badge.style === "success") badgeClass += " bg-success";
+                if (item.badge.style === "secondary") badgeClass += " bg-body-secondary text-body-secondary";
+                newListItem.innerHTML = `
+                    <div class="d-flex align-items-center gap-2">
+                        <input class="custom-form-check form-check-input mt-0" type="checkbox" ${item.checked ? "checked" : ""} aria-label="${item.label}">
+                        <span>${item.label}</span>
+                    </div>
+                    <span class="${badgeClass}">${item.badge.text}</span>
+                `;
+            }
+            tasksList.appendChild(newListItem);
+        });
+
+        // Overview Module
+        // Module Title
+        const overviewTitle = document.querySelector('#overviewTitle');
+        overviewTitle.innerHTML = data.overviewModule.title;
+        // Module List
+        const overviewList = document.querySelector('#overviewList');
+        data.overviewModule.overviewList.forEach(item => {
+            const newListItem = document.createElement('li');
+            newListItem.className = "col-12 col-sm-6 col-lg-3";
+            newListItem.innerHTML = `
+                <div class="custom-status-card card text-center p-4 border">
+                    <h2 class="h6 card-title mb-0">${item.label}</h2>
+                    <p class="card-text fs-2 fw-medium">${item.value}</p>
+                </div>
+            `;
+            overviewList.appendChild(newListItem);
+        })
+
+        // Sidebar Menu  - first 6
+        const sidebarMenu1 = document.querySelector('#sidebarMenu1');
+        const sidebarItems1 = data.sidebarMenu.items;
+
+        sidebarItems1.forEach(item => {
+            const newListItem = document.createElement('li');
+            newListItem.className = 'nav-item';
+            // If active item
+            if (item.active) {
+                newListItem.classList.add('bg-light', 'border-start', 'border-3');
+                newListItem.style.setProperty('--bs-bg-opacity', '.05');
+            }
+            const linkClass = item.active
+                ? 'nav-link d-flex align-items-center gap-2 active'
+                : 'nav-link d-flex align-items-center gap-2 text-secondary';
+            newListItem.innerHTML = `
+                <a class="${linkClass}" href="${item.url}" aria-current="page">
+                    ${item.icon}
+                    ${item.label}
+                </a>
+                `;
+            sidebarMenu1.appendChild(newListItem);
+        });
+
+        // Sidebar Menu - last 2
+        const sidebarMenu2 = document.querySelector('#sidebarMenu2');
+        const sidebarItems2 = data.sidebarMenu2.items;
+
+        sidebarItems2.forEach(item => {
+            const newListItem = document.createElement('li');
+            newListItem.className = 'nav-item';
+            // If active item
+            if (item.active) {
+                newListItem.classList.add('bg-light', 'border-start', 'border-3');
+                newListItem.style.setProperty('--bs-bg-opacity', '.05');
+            }
+            const linkClass = item.active
+                ? 'nav-link d-flex align-items-center gap-2 active'
+                : 'nav-link d-flex align-items-center gap-2 text-secondary';
+            newListItem.innerHTML = `
+                <a class="${linkClass}" href="${item.url}" aria-current="page">
+                    ${item.icon}
+                    ${item.label}
+                </a>
+                `;
+            sidebarMenu2.appendChild(newListItem);
+        });
+    } catch (error) {
+        console.warn(`Nope: ${error}`)
+        console.error(error.stack);
+    }
+}
+getData();
